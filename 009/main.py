ac625636eed0d34e09c9302888815bc3ab5bb5f4
igorkeven@hotmail.com <igorkeven@hotmail.com>
2022-11-20 10:47:18 -0300

    aula 009
    melhorando a segurança das rotas de um jeito simples

---
+++ 009/main.py
@@ -0,0 +1,75 @@
+
+
+from flask import Flask, render_template, redirect, request, flash
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'IGORKEVEN'
+
+logado = False
+
+@app.route('/')
+def home():
+    global logado
+    logado = False
+    return render_template('login.html')
+
+@app.route('/adm')
+def adm():
+    if logado == True:
+        return render_template("administrador.html")
+    if logado == False:
+        return redirect('/')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    global logado
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+
+    with open('usuarios.json') as usuariosTemp:
+        usuarios = json.load(usuariosTemp)
+        cont = 0
+        for usuario in usuarios:
+            cont += 1
+
+            if nome == 'adm' and senha == '000':
+                logado = True
+                return redirect('/adm')
+
+            if usuario['nome'] == nome and usuario['senha'] == senha:
+                return render_template("usuarios.html")
+            
+            if cont >= len(usuarios):
+                flash('USUARIO INVALIDO')
+                return redirect("/")
+
+@app.route('/cadastrarUsuario', methods=['POST'])
+def cadastrarUsuario():
+    user = []
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    user = [
+        {
+            "nome": nome,
+            "senha": senha
+        }
+    ]
+    with open('usuarios.json') as usuariosTemp:
+        usuarios = json.load(usuariosTemp)
+
+    usuarioNovo = usuarios + user
+
+    with open('usuarios.json', 'w') as gravarTemp:
+        json.dump(usuarioNovo, gravarTemp, indent=4 )
+
+    return redirect('/adm')
+
+
+
+
+
+
+if __name__ in "__main__":
+    app.run(debug=True)    
